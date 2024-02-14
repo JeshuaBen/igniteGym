4ea9399acea64ae5fb971311ee2b6a6dced2c4f8
JeshuaBen <dev.09@pbhub.com.br>
2024-02-14 16:34:44 -0300

    feat: implementing theme customization

--- App.tsx
+++ App.tsx
@@ -1,30 +1,25 @@
-import { View, Text, StatusBar } from "react-native";
+import { View, StatusBar } from "react-native";
 
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+import { NativeBaseProvider } from "native-base";
+import Loading from "@components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#202024",
-      }}
-    >
+    <NativeBaseProvider>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
 
-      {fontsLoaded ? <Text>OPA</Text> : <View />}
-    </View>
+      {fontsLoaded ? <View /> : <Loading />}
+    </NativeBaseProvider>
   );
 }
---
+++ src/components/Loading.tsx
@@ -0,0 +1,11 @@
+import { Center, Spinner } from "native-base";
+
+const Loading = () => {
+  return (
+    <Center flex={1}>
+      <Spinner />
+    </Center>
+  );
+};
+
+export default Loading;
